3c756003ed46ff0166079aa04c04e277e9e01b27
Liam <liargent@hotmail.com>
2023-09-02 15:17:01 +0100

    Add vector2 class

---
+++ src/vector2.js
@@ -0,0 +1,30 @@
+export default class Vector2 {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    static add(a, b) {
+        return new Vector2(a.x + b.x, a.y + b.y);
+    }
+
+    static sub(a, b) {
+        return new Vector2(a.x - b.x, a.y - b.y);
+    }
+
+    static div(v2, scalar) {
+        return new Vector2(v2.x / scalar, v2.y / scalar);
+    }
+
+    static mult(v2, scalar) {
+        return new Vector2(v2.x * scalar, v2.y * scalar);
+    }
+
+    magnitude() {
+        return Math.sqrt(Math.pow(this.x, 2) + Math.pow(this.y, 2));
+    }
+
+    normalised() {
+        return Vector2.div(this, this.magnitude());
+    }
+}
