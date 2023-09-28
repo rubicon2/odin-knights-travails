6341af27d84945ead9a3f81b740ee2019312ad3a
Liam <liargent@hotmail.com>
2023-09-28 12:57:16 +0100

    Add testing code

--- src/index.js
+++ src/index.js
@@ -2,11 +2,9 @@ import './style.css';
 import Knight from './knight';
 import Vector2 from './vector2';
 
-let knightA = new Knight(0, 0);
-
 // Returns moves as an array of nodes
 console.log(
-    Knight.moveMap.findPath({ x: 0, y: 0 }, { x: 7, y: 2 }, (a, b) =>
+    Knight.moveMap.findPath({ x: 0, y: 0 }, { x: 7, y: 7 }, (a, b) =>
         Vector2.sub(
             new Vector2(b.data.x, b.data.y),
             new Vector2(a.data.x, a.data.y)
@@ -15,4 +13,4 @@ console.log(
 );
 
 // Returns moves as a string as per Odin project assignment
-console.log(Knight.moves([0, 0], [7, 2]));
+console.log(Knight.moves([0, 0], [7, 7]));
