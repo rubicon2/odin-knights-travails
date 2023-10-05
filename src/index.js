4eb821953e1d85206ebc917823f61af194b15043
Liam <liargent@hotmail.com>
2023-10-05 15:40:46 +0100

    Remove redundant testing code

--- src/index.js
+++ src/index.js
@@ -1,28 +1,4 @@
 import './style.css';
-import Knight from './knight';
-import Vector2 from './vector2';
 import * as Dom from './dom';
 
 Dom.createPage(document.querySelector('body'));
-
-// Returns moves as an array of nodes
-console.log(
-    Knight.moveMap.findPath({ x: 0, y: 0 }, { x: 7, y: 7 }, (a, b) =>
-        Vector2.sub(
-            new Vector2(b.data.x, b.data.y),
-            new Vector2(a.data.x, a.data.y)
-        ).magnitude()
-    )
-);
-
-// Returns moves as a string as per Odin project assignment
-console.log(Knight.moves([0, 7], [7, 7]));
-console.log(Knight.moves([7, 7], [0, 0]));
-console.log(Knight.moves([0, 7], [7, 0]));
-console.log(Knight.moves([7, 0], [0, 7]));
-
-console.log(Knight.moves([0, 0], [0, 0]));
-console.log(Knight.moves([7, 7], [7, 7]));
-
-console.log(Knight.moves([3, 3], [0, 0]));
-console.log(Knight.moves([0, 0], [3, 3]));
